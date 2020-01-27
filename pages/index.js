faf128a18358d65c64619904073be33dde1008bd
Zhark10 <arkady.zharavin@citronium.biz>
2020-01-27 22:41:42 +0300

    component Home (func to class)

--- pages/index.js
+++ pages/index.js
@@ -4,49 +4,57 @@ import fetch from 'isomorphic-unfetch';
 import { Jumbotron, Badge, ListGroupItem, ListGroup } from 'reactstrap';
 import Layout from '../components/layout';
 
-const Home = ({ TVShows }) => (
-  <Layout>
-    <Jumbotron>
-      <h1 className="display-3">All shows</h1>
-      <p className="lead">here are all the sports TV-shows</p>
-    </Jumbotron>
+class Home extends React.Component {
+  static async getInitialProps(ctx) {
+    const res = await fetch('http://api.tvmaze.com/search/shows?q=sport');
+    const TVShows = await res.json();
 
-    <ListGroup>
-      {TVShows.map(item => (
-        <Link key={item.show.id} href="/show/[id]" as={`/show/${item.show.id}`}>
-          <ListGroupItem
-            className="justify-content-between"
-            tag="a"
-            href="#"
-            action
-          >
-            {`${item.show.name}      `}
-            <Badge pill>Rating: {item.score}</Badge>
-          </ListGroupItem>
-        </Link>
-      ))}
-    </ListGroup>
+    return { TVShows };
+  }
 
-    <style jsx>{`
-      .matches-title {
-        font-size: 24px;
-        font-weight: 700;
-        text-align: center;
-        padding: 24px;
-      }
-      .list-group-item {
-        cursor: pointer !important;
-      }
-    `}</style>
-  </Layout>
-);
+  render() {
+    const { TVShows } = this.props;
+    return (
+      <Layout>
+        <Jumbotron>
+          <h1 className="display-3">All shows</h1>
+          <p className="lead">here are all the sports TV-shows</p>
+        </Jumbotron>
 
-// eslint-disable-next-line func-names
-Home.getInitialProps = async function() {
-  const res = await fetch('http://api.tvmaze.com/search/shows?q=sport');
-  const TVShows = await res.json();
+        <ListGroup>
+          {TVShows.map(item => (
+            <Link
+              key={item.show.id}
+              href="/show/[id]"
+              as={`/show/${item.show.id}`}
+            >
+              <ListGroupItem
+                className="justify-content-between"
+                tag="a"
+                href="#"
+                action
+              >
+                {`${item.show.name}      `}
+                <Badge pill>Rating: {item.score}</Badge>
+              </ListGroupItem>
+            </Link>
+          ))}
+        </ListGroup>
 
-  return { TVShows };
-};
+        <style jsx>{`
+          .matches-title {
+            font-size: 24px;
+            font-weight: 700;
+            text-align: center;
+            padding: 24px;
+          }
+          .list-group-item {
+            cursor: pointer !important;
+          }
+        `}</style>
+      </Layout>
+    );
+  }
+}
 
 export default Home;

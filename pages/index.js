568e026ddd92d5648af92ef3df329ae0977f3ee2
Zhark10 <arkady.zharavin@citronium.biz>
2020-05-11 17:49:16 +0300

    class component to fc

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
-import { Jumbotron, Badge, ListGroupItem, ListGroup } from 'reactstrap';
+import { Jumbotron, Badge, ListGroupItem, ListGroup, Input } from 'reactstrap';
 import AwesomeDebouncePromise from 'awesome-debounce-promise';
 import useConstant from 'use-constant';
 import { useAsync } from 'react-async-hook';
 import Layout from '../components/layout';
 
-class Home extends React.Component {
-  static async getInitialProps(ctx) {
-    const res = await fetch('http://api.tvmaze.com/search/shows?q=sport');
-    const TVShows = await res.json();
-
-    return { TVShows };
-  }
-
-  useDebouncedSearch = searchFunction => {
+const Home = ({ TVShows }) => {
+  const useDebouncedSearch = searchFunction => {
     const [inputText, setInputText] = React.useState('');
 
     const debouncedSearchFunction = useConstant(() =>
@@ -36,49 +29,55 @@ class Home extends React.Component {
     };
   };
 
-  render() {
-    const { TVShows } = this.props;
-    return (
-      <Layout>
-        <Jumbotron>
-          <h1 className="display-3">All shows</h1>
-          <p className="lead">here are all the sports TV-shows</p>
-        </Jumbotron>
+  return (
+    <Layout>
+      <Jumbotron>
+        <h1 className="display-3">All shows</h1>
+        <p className="lead">here are all the sports TV-shows</p>
+      </Jumbotron>
 
-        <ListGroup>
-          {TVShows.map(item => (
-            <Link
-              key={item.show.id}
-              href="/show/[id]"
-              as={`/show/${item.show.id}`}
+      <Input placeholder="category" />
+
+      <ListGroup>
+        {TVShows.map(item => (
+          <Link
+            key={item.show.id}
+            href="/show/[id]"
+            as={`/show/${item.show.id}`}
+          >
+            <ListGroupItem
+              className="justify-content-between"
+              tag="a"
+              href="#"
+              action
             >
-              <ListGroupItem
-                className="justify-content-between"
-                tag="a"
-                href="#"
-                action
-              >
-                {`${item.show.name}      `}
-                <Badge pill>Rating: {item.score}</Badge>
-              </ListGroupItem>
-            </Link>
-          ))}
-        </ListGroup>
+              {`${item.show.name}      `}
+              <Badge pill>Rating: {item.score}</Badge>
+            </ListGroupItem>
+          </Link>
+        ))}
+      </ListGroup>
 
-        <style jsx>{`
-          .matches-title {
-            font-size: 24px;
-            font-weight: 700;
-            text-align: center;
-            padding: 24px;
-          }
-          .list-group-item {
-            cursor: pointer !important;
-          }
-        `}</style>
-      </Layout>
-    );
-  }
-}
+      <style jsx>{`
+        .matches-title {
+          font-size: 24px;
+          font-weight: 700;
+          text-align: center;
+          padding: 24px;
+        }
+        .list-group-item {
+          cursor: pointer !important;
+        }
+      `}</style>
+    </Layout>
+  );
+};
+
+Home.getInitialProps = async () => {
+  const res = await fetch('http://api.tvmaze.com/search/shows?q=sport');
+  const TVShows = await res.json();
+
+  return { TVShows };
+};
 
 export default Home;
